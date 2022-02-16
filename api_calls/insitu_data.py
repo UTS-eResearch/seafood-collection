2b594c9423ae6bfcf1ca8d6c65b435f877ed0779
Simon Kruik <simon.kruik@uts.edu.au>
2022-02-16 15:34:26 +1100

    Added split_row function to rearrange data to match other format

--- api_calls/insitu_data.py
+++ api_calls/insitu_data.py
@@ -4,12 +4,17 @@
 ####
 import requests
 import json
+import csv
+from io import StringIO # For converting string to file object to use with CSV reader
+
+#### Goal Headers: id, MeasurementTime, SensorName, SensorDescription [location name], SesnorDetails, Type, Units, CurrentValue, Lat, Long
+
 
 locations = {
-    "A1B":"",
-    "A2":"",
-    "A3":"",
-    "A4":""
+    "A1B":"Oyster Harbour North",
+    "A2":"Oyster Harbour Central",
+    "A3":"Oyster Harbour East",
+    "A4":"Oyster Harbour South"
 }
 
 sensor_lat_lons = {
@@ -52,14 +57,40 @@ def get_devices():
     return device_id_list
 
 def get_historical_readings(device_id, start_date, end_date):
+    readings_matrix = []
     query_params = [("date_from",start_date),("date_to",end_date),("format","csv")]
     endpoint = "devices/" +  device_id + "/readings"
-    readings = api_get(endpoint, query_params)
-    return readings
+    readings_string = api_get(endpoint, query_params)
+    csv_file = StringIO(readings_string)
+    reader = csv.reader(csv_file, delimiter=",")
+    for row in reader:
+        readings.append(row)
+    return readings_matrix
+
+def print_header_row(device_id, start_date, end_date):
+    readings_matrix = []
+    query_params = [("date_from",start_date),("date_to",end_date),("format","csv")]
+    endpoint = "devices/" +  device_id + "/readings"
+    readings_string = api_get(endpoint, query_params)
+    csv_file = StringIO(readings_string)
+    reader = csv.reader(csv_file, delimiter=",")
+    for row in reader:
+        print(row)
+        break
+
+def split_readings(row, device, endpoint, location_name, lat, long, id_index, time_index,depth_index,temp_index,conductivity_index,salinity_index,water_density_index):
+    salinity_row = [row[id_index],row[time_index],device,location_name,endpoint,"salinity","parts per thousand",row[salinity_index],lat,long]
+    depth_row = [row[id_index],row[time_index],device,location_name,endpoint,"depth","mH2O",row[depth_index],lat,long]
+    temp_row = [row[id_index],row[time_index],device,location_name,endpoint,"temperature","degrees Celsius",row[temp_index],lat,long]
+    conductivity_row = [row[id_index],row[time_index],device,location_name,endpoint,"conductivity","millisiemens per meter",row[conductivity_index],lat,long]
+    water_density_row = [row[id_index],row[time_index],device,location_name,endpoint,"water density","grams per Litre",row[water_density_index],lat,long]
+    return [salinity_row,depth_row,temp_row,conductivity_row,water_density_row]
 
 def write_months_readings(start_date,end_date):
     devices = get_devices()
     for device_id in devices:
-        readings_string = get_historical_readings(device_id, start_date, end_date)
+        readings_matrix = get_historical_readings(device_id, start_date, end_date)
         with open('out_WA_' + device_id + "_" + start_date + "_" + end_date + '.csv','w') as file:
-            file.write(readings_string)
+            for row in readings_matrix:
+                file.write(','.join(row))
+                file.write('\n')
