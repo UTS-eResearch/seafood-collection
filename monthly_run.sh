30b0e192c1338384912730bef5996038c6192bf7
Simon Kruik <simon.kruik@uts.edu.au>
2022-07-25 13:02:45 +1000

    Added stage 1.5 to monthly script

--- monthly_run.sh
+++ monthly_run.sh
@@ -4,15 +4,18 @@
 # 2022-07-21
 #
 
-PROGRAM_NAME=$0
+# For using in bash, get start and dates with `date +%Y-%m | sed "s/-/ /g"` and `date +%Y-%m -d 'next month' | sed "s/-/ /g"`
 
+PROGRAM_NAME=$0
 function usage {
 	echo "usage: $PROGRAM_NAME [-w|--insitu] [-n|--ict] [-o|-ocfl_repo <location>] [-a|--aws_sync_script <location>] start_date end_date"
+	echo "start_date and end_date should be in format yyyy-mm (ISO 8601), e.g. 2022-02"
 	echo ""
 	echo "Environment Variables:"
 	echo "python3_location=<filepath to your python3 installation> (defaults to executing which python3 to get filepath from PATH variable)"
-	echo "api_file_location=<filepath to the insitu_api.py and eagleio_api.py scripts> (defaults to pwd)"
-	
+	echo "api_file_location=<filepath to the repo containing insitu_api.py and eagleio_api.py scripts> (defaults to pwd)"
+
+
 	exit 1
 }
 
@@ -25,17 +28,21 @@ do
 case $1 in
  -w|--insitu)
 	echo "Getting the Insitu data from WA"
+	INSITU=true
 	;;
  -n|--ict)
 	echo "Getting the ICT data from NSW"
+	ICT=true
 	;;
  -o|--ocfl_repo)
 	shift
 	echo "OCFL Repo located at: ${1}"
+	OCFL_REPO=$1
 	;;
  -a|--aws_sync_script)
 	shift
 	echo "AWS Sync Script located at: ${1}"
+	AWS_SCRIPT=$1
 	;;
  --)
 	shift
@@ -44,6 +51,9 @@ esac
 shift
 done
 
+START_DATE=$1
+END_DATE=$2
+
 # Stage 1: Pulling the data down
 if [[ -z "${python3_location}" ]]; then
 	PYTHON_PATH=`which python3`
@@ -51,7 +61,7 @@ else
 	PYTHON_PATH="${python3_location}"
 fi
 
-echo "Running with ${PYTHON_PATH}"
+echo "Looking for python3 at ${PYTHON_PATH}"
 
 
 if [[ -z "${api_file_location}" ]]; then
@@ -62,6 +72,40 @@ fi
 
 echo "Looking for API scripts in ${API_PATH}"
 
+if [[ $ICT -eq true ]]; then
+
+	echo "Clearing all files in ${API_PATH}/api_calls/ict_data/"
+	rm -f ${API_PATH}/api_calls/ict_data/*
+	$PYTHON_PATH $API_PATH/api_calls/eagleio_data.py $START_DATE $END_DATE
+
+fi
+
+if [[ $INSITU -eq true ]]; then
+
+	echo "Clearing all files in ${API_PATH}/api_calls/insitu_data/"
+	rm -f ${API_PATH}/api_calls/insitu_data/*
+	$PYTHON_PATH $API_PATH/api_calls/insitu_data.py $START_DATE $END_DATE
+
+fi
+
+
+# Stage 1.5: Packaging the data into RO-Crates
+
+echo "Clearing all files in ${API_PATH}/sensor-data/"
+rm -f ${API_PATH}/sensor-data/*
+cd ${API_PATH}
+
+if [[ $ICT -eq true ]]; then
+
+	$PYTHON_PATH $API_PATH/open_data.py --api --folder $API_PATH/api_calls/ict_data/
+
+fi
+
+if [[ $INSITU -eq true ]]; then
+
+	$PYTHON_PATH $API_PATH/open_data.py --api --folder $API_PATH/api_calls/insitu_data/
+
+fi
 
 # Stage 2: Depositing the data in OCFL repo
 
@@ -72,3 +116,5 @@ echo "Looking for API scripts in ${API_PATH}"
 if [[ "$#" -eq 0 ]]; then
 	usage
 fi
+
+exit 0
